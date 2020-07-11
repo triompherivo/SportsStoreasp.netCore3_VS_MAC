e9939a6e2c1113c281d0450067ca03f99f5feeec
triomphe ramandalahy <triompherivo@gmail.com>
2020-07-11 12:41:12 +0200

    Chapter 8: SportsStore navigation and cart on my macbook pro

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -32,6 +32,9 @@ namespace SportsStore
                 );
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,15 +43,23 @@ namespace SportsStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("catpage", "{category}/Page{productPage:int}",
+                    new { Controller = "Home", action = "Index" });
+                endpoints.MapControllerRoute("page", "Page{productPage:int}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
+                endpoints.MapControllerRoute("category", "{category}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapControllerRoute("pagination",
                     "Products/Page{productPage}",
-                    new {Controller = "Home", action = "Index"}
+                    new {Controller = "Home", action = "Index",productPage = 1}
                 );
                 endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
             });
             SeedData.EnsurePopulated(app);
         }
